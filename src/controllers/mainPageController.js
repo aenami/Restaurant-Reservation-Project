dad817c82d1ebc857061492de4b60e1afdb5fdc6
Julian <juliannandres05@gmail.com>
2025-11-18 13:49:10 -0500

    Menu terminado

--- src/controllers/mainPageController.js
+++ src/controllers/mainPageController.js
@@ -1,14 +1,14 @@
 // Importamos el modelo de platillos
-const Platillo = require('../models/platilloModel')
+const { Platillo } = require('../models/platilloModel')
 
 const baseRender = async (req, res) =>{
     try {
         const allDishes = await Platillo.getAll()
 
         // Filtrar por tipo_platillo
-        const breakfast = allDishes.filter(p => p.tipo_platillo === 1)
-        const lunch = allDishes.filter(p => p.tipo_platillo === 2)
-        const dinner = allDishes.filter(p => p.tipo_platillo === 3)
+        const breakfast = allDishes.filter(p => p.tipo_platillo === "Desayuno")
+        const lunch = allDishes.filter(p => p.tipo_platillo === "Almuerzo")
+        const dinner = allDishes.filter(p => p.tipo_platillo === "Cena")
 
         res.render('mainPage.ejs', {
             message: null,
--- src/models/platilloModel.js
+++ src/models/platilloModel.js
@@ -4,9 +4,17 @@ const Platillo = {
     //---Consultas a ejecutar de mi modelo
     async getAll(){
         try {
-            // Consulta SQL
-            const[rows] = await pool.query('SELECT * FROM platillos')
-            return rows
+            const query = `
+                SELECT p.id_platillo, p.nombre_platillo, p.descripcion_platillo, 
+                    p.precio_platillo, t.tipo_platillo,
+                    i.ruta_imagen
+                FROM platillos p
+                INNER JOIN tipo_platillos t ON t.id_platillo_tipo = p.id_tipo_platillo
+                LEFT JOIN imagenes_platillo i ON i.id_platillo_imagen = p.id_platillo
+            `;
+
+            const [rows] = await pool.query(query);
+            return rows;
         } catch (error) {
             console.log(`ERROR: ${error}`)
         }
